318ff6bce64d8f3d985abb4c2c9f5f1b55174837
TavinScapi <tavinhoscapi@gmail.com>
2025-03-08 01:24:08 -0300

    a

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".innovasfera",
+  `       Durante meu curso técnico em Desenvolvimento de Sistemas e enquanto curso Ciência da Computação, 
+                            desenvolvi projetos utilizando HTML, CSS e JavaScript. Trabalhei na criação de interfaces web responsivas, 
+                            aprimorando minhas habilidades em estilização, interatividade e desenvolvimento front-end.
+                        `,
+  "Desenvolvedor Full Stack",
+  "HTML/CSS/JS",
+  "2023 - No momento"
+);
+
+hoverChangeExperience(
+  ".codelab",
+  `        Durante meu curso técnico, tive a oportunidade de aprender e trabalhar com Java. Desenvolvi projetos que 
+        envolveram a criação de aplicações e lógica de programação, aprimorando minhas habilidades em orientação a objetos 
+        e construção de sistemas robustos.`,
+  "Desenvolvedor Full Stack",
+  "JAVA",
+  "2023 - No momento"
+);
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `Durante meu curso técnico em Desenvolvimento de Sistemas, aprendi a trabalhar com MySQL, desenvolvendo e manipulando bancos de dados relacionais. 
+    Trabalhei na criação de tabelas, consultas complexas, inserções e gerenciamento de dados, além de integrar o banco com aplicações para otimizar o processamento de informações.`,
+  "Desenvolvedor Full Stack",
+  "MYSQL",
+  "2023 - No momento"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `    Durante meu curso técnico em Desenvolvimento de Sistemas, aprendi a usar Git e GitHub para controle de versão e colaboração em projetos. 
+    Utilizei essas ferramentas para gerenciar o histórico de código, realizar merges, trabalhar com branches e colaborar com outros desenvolvedores, garantindo o versionamento adequado e a integração contínua em meus projetos.
+`,
+  "Desenvolvedor Full Stack",
+  "GIT/GITHUB",
+  "2023 - No momento"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `    Durante meu curso técnico em Desenvolvimento de Sistemas, comecei a explorar o React para o desenvolvimento de interfaces de usuário dinâmicas e interativas. 
+    Trabalhei com componentes reutilizáveis, gerenciamento de estado e integração com APIs, aprimorando minhas habilidades em desenvolvimento front-end e criando aplicações web mais eficientes e escaláveis.
+`,
+  "Desenvolvedor Full Stack",
+  "REACT",
+  "2023 - No momento"
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `    Durante meu curso técnico em Desenvolvimento de Sistemas, aprendi a trabalhar com Python em diversos projetos. 
+    Utilizei a linguagem para automatização de tarefas, desenvolvimento de scripts e pequenas aplicações, além de trabalhar com bibliotecas como Flask e Django para o desenvolvimento de soluções web e APIs.
+`,
+  "Desenvolvedor Full Stack",
+  "PYTHON",
+  "2023 - No momento"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
